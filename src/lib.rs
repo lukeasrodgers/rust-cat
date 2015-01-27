4844df4dff9f223e3b626c2b27fd9b702bb14e3e
Luke Rodgers <lukeasrodgers@gmail.com>
2015-01-26 23:43:45 -0500

    handles vt options with bn and s not terrible

--- src/lib.rs
+++ src/lib.rs
@@ -27,23 +27,11 @@ fn cat_files(v: &Vec<String>, options: &getopts::Matches) {
     let mut linenum = 1u32;
     for filename in v.iter() {
         let path = Path::new(filename);
-
-        if options.opt_present("v") || options.opt_present("t") {
-            let mut file = BufferedReader::new(File::open(&path));
-            loop {
-                match file.read_byte() {
-                    Ok(b) => { print_byte(b, options) },
-                    Err(f) => { break; }
-                }
-            }
-        }
-        else {
-            let mut file = BufferedReader::new(File::open(&path));
-            for line in file.lines() {
-                let (a, b) = handle_line(line, &mut printempty, &mut linenum, options);
-                printempty = a;
-                linenum = b;
-            }
+        let mut file = BufferedReader::new(File::open(&path));
+        for line in file.lines() {
+            let (a, b) = handle_line(line, &mut printempty, &mut linenum, options);
+            printempty = a;
+            linenum = b;
         }
     }
 }
@@ -84,21 +72,44 @@ fn handle_line<'a>(
 fn print_string<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Matches) -> &'a mut u32 {
     if options.opt_present("b") {
         if !is_empty(s) {
-            print!("     {}  {}", linenum, s);
+            print_numbered(s, linenum, options);
             *linenum = *linenum + 1;
         }
         else {
-            print!("{}", s);
+            print_unnumbered(s, options);
         }
     }
     else if options.opt_present("n") {
-        print!("     {}  {}", linenum, s);
+        print_numbered(s, linenum, options);
         *linenum = *linenum + 1;
+    }
+    else {
+        print_unnumbered(s, options);
+    }
+    linenum
+}
+
+fn print_numbered<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Matches) {
+    if options.opt_present("v") || options.opt_present("t") {
+        print!("      {} ", linenum);
+        for b in s.as_bytes().iter() {
+            print_byte(b, options);
+        }
+    }
+    else {
+        print!("     {}  {}", linenum, s);
+    }
+}
+
+fn print_unnumbered(s: &String, options: &getopts::Matches) {
+    if options.opt_present("v") || options.opt_present("t") {
+        for b in s.as_bytes().iter() {
+            print_byte(b, options);
+        }
     }
     else {
         print!("{}", s);
     }
-    linenum
 }
 
 fn is_empty(line: &String) -> bool {
@@ -115,17 +126,17 @@ fn is_empty(line: &String) -> bool {
     }
 }
 
-fn print_byte(b: u8, options: &getopts::Matches) {
-    if b > 31 || b == 10 || b == 9 {
-        if options.opt_present("t") && b == 9 {
-            print!("^{}", (b + 64) as char);
+fn print_byte(b: &u8, options: &getopts::Matches) {
+    if *b > 31 || *b == 10 || *b == 9 {
+        if options.opt_present("t") && *b == 9 {
+            print!("^{}", (*b + 64) as char);
         }
         else {
-            print!("{}", b as char);
+            print!("{}", *b as char);
         }
     }
     else {
-        print!("^{}", (b + 64) as char);
+        print!("^{}", (*b + 64) as char);
     }
 }
 
