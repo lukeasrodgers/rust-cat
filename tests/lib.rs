d91c988581e00995bf2eae131711bd4f4c359256
Luke Rodgers <lukeasrodgers@gmail.com>
2015-01-28 19:54:03 -0500

    Include MIT license from coreutils in tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,3 +1,24 @@
+// Copyright (c) Jordi Boggiano
+// With modifications by Luke Rodgers
+// https://github.com/uutils/coreutils/blob/2f0d8c89c94ee4b6c1d911c48d6e56f002e2def7/test/cat.rs
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #![allow(unstable)]
 
 use std::io::process::Command;
