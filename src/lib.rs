fbe8d6b7668c001a1bf60adab8b3737685bceead
Luke Rodgers <lukeasrodgers@gmail.com>
2015-02-02 00:35:48 -0500

    mostly working, squeeze broken all tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,13 @@ pub fn cat(v: &Vec<String>, options: &getopts::Matches) {
 }
 
 fn cat_files(v: &Vec<String>, options: &getopts::Matches) {
+    let mut linenum = 1u32;
     for filename in v.iter() {
         let path = Path::new(filename);
         match File::open(&path) {
             Ok(fh) => {
                 let mut file = BufferedReader::new(fh);
-                cat_file(file, options);
+                linenum = cat_file(file, options, linenum);
             },
             Err(f) => {
                 println!("{}", f);
@@ -43,18 +44,21 @@ fn cat_files(v: &Vec<String>, options: &getopts::Matches) {
 }
 
 fn cat_stdin(options: &getopts::Matches) {
-    let mut printempty: bool = false;
     let mut linenum = 1u32;
+    let mut printempty: bool = false;
     for line in old_io::stdin().lock().lines() {
-        let (a, b) = handle_line(line.unwrap(), &mut printempty, &mut linenum, options);
+        let (a, b) = handle_line(line.unwrap(), &mut printempty, linenum, options);
         printempty = a;
         linenum = b;
     }
 }
 
-fn cat_file(mut file: old_io::BufferedReader<std::old_io::fs::File>, options: &getopts::Matches) {
+fn cat_file<'a>(
+    mut file: old_io::BufferedReader<std::old_io::fs::File>,
+    options: &getopts::Matches,
+    mut linenum: u32, 
+    ) -> u32 {
     let mut printempty: bool = false;
-    let mut linenum = 1u32;
     let mut read_buf = [0, 4096];
     let mut out_buf: Vec<u8> = vec![];
     loop {
@@ -62,53 +66,77 @@ fn cat_file(mut file: old_io::BufferedReader<std::old_io::fs::File>, options: &g
             Ok(n) => {
                 for c in read_buf[..n].iter() {
                     out_buf.push(*c);
-                    // if end of line
-                    // print_byte(c, options);
-                    if *c == 10u8 {
-                        let s = String::from_utf8(out_buf.clone()).unwrap();
-                        let (a, b) = handle_line(s, &mut printempty, &mut linenum, options);
+                    if is_nl(c) {
+                        let (a, b) = handle_buf(&out_buf, &mut printempty, linenum, options);
                         printempty = a;
                         linenum = b;
                         out_buf.clear();
                     }
-                    // print_byte(c, options);
                 }
             },
-            Err(f) => { break }
+            Err(f) => {
+                let mut o = 0u32;
+                // print_buf(&out_buf, &mut o, options);
+
+                if out_buf.len() > 0 {
+                    handle_buf(&out_buf, &mut printempty, linenum, options);
+                }
+                break
+            }
         }
     }
-    // leftover
-    let s = String::from_utf8(out_buf.clone()).unwrap();
-    handle_line(s, &mut printempty, &mut linenum, options);
+    linenum
 }
 
 fn handle_line<'a>(
     linestr: String,
     printempty: &'a mut bool,
-    linenum: &'a mut u32,
+    mut linenum: u32,
     options: &getopts::Matches) -> (bool, u32) {
     if options.opt_present("s") {
         if !is_empty(&linestr) {
             if *printempty == true {
                 println!("");
             }
-            *linenum = *print_string(&linestr, linenum, options);
+            linenum = print_string(&linestr, linenum, options);
         }
         else {
             *printempty = true;
         }
     }
     else {
-        *linenum = *print_string(&linestr, linenum, options);
+        linenum = print_string(&linestr, linenum, options);
     }
-    (*printempty, *linenum)
+    (*printempty, linenum)
 }
 
-fn print_string<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Matches) -> &'a mut u32 {
+fn handle_buf<'a>(
+    out_buf: &Vec<u8>,
+    printempty: &'a mut bool,
+    mut linenum: u32,
+    options: &getopts::Matches) -> (bool, u32) {
+    if options.opt_present("s") {
+        if !is_empty_out_buf(out_buf) {
+            if *printempty == true {
+                println!("");
+            }
+            linenum = print_buf(&out_buf, linenum, options);
+        }
+        else {
+            *printempty = true;
+        }
+    }
+    else {
+        linenum = print_buf(&out_buf, linenum, options);
+    }
+    (*printempty, linenum)
+}
+
+fn print_string<'a>(s: &String, mut linenum: u32, options: &getopts::Matches) -> u32 {
     if options.opt_present("b") {
         if !is_empty(s) {
             print_numbered(s, linenum, options);
-            *linenum = *linenum + 1;
+            linenum = linenum + 1;
         }
         else {
             print_unnumbered(s, options);
@@ -116,7 +144,7 @@ fn print_string<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Matches
     }
     else if options.opt_present("n") {
         print_numbered(s, linenum, options);
-        *linenum = *linenum + 1;
+        linenum = linenum + 1;
     }
     else {
         print_unnumbered(s, options);
@@ -124,7 +152,27 @@ fn print_string<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Matches
     linenum
 }
 
-fn print_numbered<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Matches) {
+fn print_buf<'a>(out_buf: &Vec<u8>, mut linenum: u32, options: &getopts::Matches) -> u32 {
+    if options.opt_present("b") {
+        if !is_empty_out_buf(out_buf) {
+            print_numbered_buf(out_buf, linenum, options);
+            linenum = linenum + 1;
+        }
+        else {
+            print_unnumbered_buf(out_buf, options);
+        }
+    }
+    else if options.opt_present("n") {
+        print_numbered_buf(out_buf, linenum, options);
+        linenum = linenum + 1;
+    }
+    else {
+        print_unnumbered_buf(out_buf, options);
+    }
+    linenum
+}
+
+fn print_numbered<'a>(s: &String, linenum: u32, options: &getopts::Matches) {
     if options.opt_present("v") || options.opt_present("t") {
         print!("     {}\t", linenum);
         for b in s.as_bytes().iter() {
@@ -136,6 +184,15 @@ fn print_numbered<'a>(s: &String, linenum: &'a mut u32, options: &getopts::Match
     }
 }
 
+fn print_numbered_buf<'a>(out_buf: &Vec<u8>, linenum: u32, options: &getopts::Matches) {
+    // if options.opt_present("v") || options.opt_present("t") {
+        print!("     {}\t", linenum);
+        for b in out_buf.iter() {
+            print_byte(b, options);
+        }
+    // }
+}
+
 fn print_unnumbered(s: &String, options: &getopts::Matches) {
     if options.opt_present("v") || options.opt_present("t") {
         for b in s.as_bytes().iter() {
@@ -145,6 +202,17 @@ fn print_unnumbered(s: &String, options: &getopts::Matches) {
     else {
         print!("{}", s);
     }
+}
+
+fn print_unnumbered_buf(out_buf: &Vec<u8>, options: &getopts::Matches) {
+    // if options.opt_present("v") || options.opt_present("t") {
+        for b in out_buf.iter() {
+            print_byte(b, options);
+        }
+    // }
+    // else {
+        // print!("{}", s);
+    // }
 }
 
 fn is_empty(line: &String) -> bool {
@@ -161,18 +229,54 @@ fn is_empty(line: &String) -> bool {
     }
 }
 
+fn is_empty_out_buf(v: &Vec<u8>) -> bool {
+    if v.len() > 1 {
+        false
+    }
+    else if is_nl(&v[0]) {
+        true
+    }
+    else {
+        false
+    }
+}
+
+fn is_nl(c: &u8) -> bool {
+    *c == 10u8
+}
+
 fn print_byte(b: &u8, options: &getopts::Matches) {
-    if *b > 31 || *b == 10 || *b == 9 {
+    if (*b > 31 && *b < 127) || *b == 10 || *b == 9 {
         if options.opt_present("t") && *b == 9 {
-            print!("^{}", (*b + 64) as char);
+            print_tab();
         }
         else {
             print!("{}", *b as char);
         }
     }
     else {
-        print!("^{}", (*b + 64) as char);
+        if options.opt_present("v") || options.opt_present("t") {
+            if *b < 32 {
+                print!("^{}", (*b + 64) as char);
+            }
+            else if *b == 127 {
+                // del char
+                print!("^?");
+            }
+            else if *b > 127 {
+                if *b == 255 {
+                    print!("M-^?");
+                }
+                else {
+                    print!("M-{}", (*b - 128) as char);
+                }
+            }
+        }
     }
+}
+
+fn print_tab() {
+    print!("^I");
 }
 
 #[cfg(test)]
--- tests/lib.rs
+++ tests/lib.rs
@@ -40,7 +40,7 @@ fn test_output_multi_files_print_all_chars() {
 
     let out = str::from_utf8(po.output.as_slice()).unwrap();
     assert_eq!(out,
-               "     1\tabcde$\n     2\tfghij$\n     3\tklmno$\n     4\tpqrst$\n     5\tuvwxyz$\n     6\t^@^A^B^C^D^E^F^G^H^I$\n     7\t^K^L^M^N^O^P^Q^R^S^T^U^V^W^X^Y^Z^[^\\^]^^^_ !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~^?M-^@M-^AM-^BM-^CM-^DM-^EM-^FM-^GM-^HM-^IM-^JM-^KM-^LM-^MM-^NM-^OM-^PM-^QM-^RM-^SM-^TM-^UM-^VM-^WM-^XM-^YM-^ZM-^[M-^\\M-^]M-^^M-^_M- M-!M-\"M-#M-$M-%M-&M-\'M-(M-)M-*M-+M-,M--M-.M-/M-0M-1M-2M-3M-4M-5M-6M-7M-8M-9M-:M-;M-<M-=M->M-?M-@M-AM-BM-CM-DM-EM-FM-GM-HM-IM-JM-KM-LM-MM-NM-OM-PM-QM-RM-SM-TM-UM-VM-WM-XM-YM-ZM-[M-\\M-]M-^M-_M-`M-aM-bM-cM-dM-eM-fM-gM-hM-iM-jM-kM-lM-mM-nM-oM-pM-qM-rM-sM-tM-uM-vM-wM-xM-yM-zM-{M-|M-}M-~M-^?");
+               "     1\tabcde\n     2\tfghij\n     3\tklmno\n     4\tpqrst\n     5\tuvwxyz\n     6\t^@^A^B^C^D^E^F^G^H^I\n     7\t^K^L^M^N^O^P^Q^R^S^T^U^V^W^X^Y^Z^[^\\^]^^^_ !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~^?M-^@M-^AM-^BM-^CM-^DM-^EM-^FM-^GM-^HM-^IM-^JM-^KM-^LM-^MM-^NM-^OM-^PM-^QM-^RM-^SM-^TM-^UM-^VM-^WM-^XM-^YM-^ZM-^[M-^\\M-^]M-^^M-^_M- M-!M-\"M-#M-$M-%M-&M-\'M-(M-)M-*M-+M-,M--M-.M-/M-0M-1M-2M-3M-4M-5M-6M-7M-8M-9M-:M-;M-<M-=M->M-?M-@M-AM-BM-CM-DM-EM-FM-GM-HM-IM-JM-KM-LM-MM-NM-OM-PM-QM-RM-SM-TM-UM-VM-WM-XM-YM-ZM-[M-\\M-]M-^M-_M-`M-aM-bM-cM-dM-eM-fM-gM-hM-iM-jM-kM-lM-mM-nM-oM-pM-qM-rM-sM-tM-uM-vM-wM-xM-yM-zM-{M-|M-}M-~M-^?");
 }
 
 #[test]
